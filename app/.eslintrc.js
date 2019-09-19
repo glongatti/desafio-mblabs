1fdf3aec0c1aef6232177ff51c9ce108f173b521
Gabriel <gabriel.augusto007@gmail.com>
2019-09-19 05:44:53 -0300

    - Adicionado arquivo Home - Adicionado modulo react-native-router-flux - Adicionado arquivo de roteamento

--- app/.eslintrc.js
+++ app/.eslintrc.js
@@ -1,4 +1,9 @@
 module.exports = {
   root: true,
   extends: '@react-native-community',
+  {
+  "env": {
+    "jest/globals": true
+  }
+}
 };
---
+++ app/src/home.js
@@ -0,0 +1,22 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React, { Fragment } from 'react';
+import { Text, View } from 'react-native';
+
+const Home = () => {
+  return (
+    <View>
+      <Text>
+        Home
+      </Text>
+    </View>
+  );
+};
+
+export default Home;
